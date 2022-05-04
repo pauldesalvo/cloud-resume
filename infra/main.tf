dec252679fe539716904816390fabab1ecd84425
Paul De Salvo <desalvopaulj@gmail.com>
2022-05-03 18:49:28 -0700

    fix lambda api gateway integration

--- infra/main.tf
+++ infra/main.tf
@@ -216,11 +216,56 @@ resource "aws_iam_role" "iam_for_lambda" {
 EOF  
 }
 
-resource "aws_lambda_function" "dynodb-lambda-function" {
-  function_name = "lambda-dyno"
-  filename      = "~/cloud-resume/api/lambda.zip"
-  role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "lambda.handler"
-  runtime       = "python3.8"
+data "archive_file" "lambda-zip" {
+  type        = "zip"
+  output_path = "/tmp/lambda_zip_file.zip"
+  source {
+    content = file("~/cloud-resume/infra/lambda/lambda.py")
+    filename = "lambda.py"
+  }
 }
+
+resource "aws_lambda_function" "dynodb-lambda-function" {
+  function_name    = "dynodb-lambda-function"
+  filename         = "${data.archive_file.lambda-zip.output_path}"
+  role             = aws_iam_role.iam_for_lambda.arn
+  handler          = "dynodb-lambda-function.lambda_handler"
+  source_code_hash = "${data.archive_file.lambda-zip.output_base64sha256}"
+  runtime          = "python3.8"
+}
+
+resource "aws_apigatewayv2_api" "lambda-api" {
+  name          = "v2-http-api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_stage" "lambda-stage" {
+  api_id      = aws_apigatewayv2_api.lambda-api.id
+  name        = "$default"
+  auto_deploy = true
+}
+
+resource "aws_apigatewayv2_integration" "lambda-integration" {
+  api_id               = aws_apigatewayv2_api.lambda-api.id
+  integration_type     = "AWS_PROXY"
+  integration_method   = "POST"
+  integration_uri      = aws_lambda_function.dynodb-lambda-function.invoke_arn
+  passthrough_behavior = "WHEN_NO_MATCH"
+}
+
+resource "aws_apigatewayv2_route" "lambda_route" {
+  api_id    = aws_apigatewayv2_api.lambda-api.id
+  route_key = "GET /{proxy+}"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda-integration.id}"
+}
+
+resource "aws_lambda_permission" "api-gw" {
+  statement_id  = "AllowedExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.dynodb-lambda-function.arn
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.lambda-api.execution_arn}/*/*/*"
+
+}
+
 
